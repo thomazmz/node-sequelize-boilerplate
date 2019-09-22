f8b7faeff30764b94157fdbaf7e88559dc83d334
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-09-22 00:28:24 -0300

    Add Nodemon in the development environment setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const app = require('./app/App');
 
-app.express.listen(3030, () => {
-    console.log('Successfully listening on port 3030');
+app.listen(3030, () => {
     app.authenticate();
 });
