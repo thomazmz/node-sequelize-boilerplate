afcb64a4edd6e5c668a30e89466345df3c05f39d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-09-22 00:47:56 -0300

    Start using Jest for test implementation

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,47 @@
- module.exports = {
-  username : "docker",
-  password : "docker",
-  database : "docker",
-  host : "127.0.0.1",
-  dialect: "postgres",
-  operatorsAliases : false,
-  logging : false,
-  define : {
-    timestamps : true,
-    underscored : true,
-    underscoreAll : true,
-    freezeTableName: false
+module.exports = {
+  development : {
+    host : "127.0.0.1",
+    database : "docker",
+    username : "docker",
+    password : "docker",
+    dialect: "postgres",
+    operatorsAliases : false,
+    logging : false,
+    define : {
+      timestamps : true,
+      underscored : true,
+      underscoreAll : true,
+      freezeTableName: false
+    }
+  },
+  test : {
+    host : "127.0.0.1",
+    database : "docker-test",
+    username : "docker",
+    password : "docker",
+    dialect: "postgres",
+    operatorsAliases : false,
+    logging : false,
+    define : {
+      timestamps : true,
+      underscored : true,
+      underscoreAll : true,
+      freezeTableName: false
+    }
+  },
+  production : {
+    host : process.env.POSTGRES_HOST,
+    database : process.env.POSTGRES_DATABASE,
+    username : process.env.POSTGRES_USERNAME,
+    password : process.env.POSTGRES_PASSWORD,
+    dialect: "postgres",
+    operatorsAliases : false,
+    logging : false,
+    define : {
+      timestamps : true,
+      underscored : true,
+      underscoreAll : true,
+      freezeTableName: false
+    }
   }
 }
---
+++ tests/unit/firstTest.test.js
@@ -0,0 +1,6 @@
+it('should  sum two values', () => {
+    const a = 5;
+    const b = 4;
+    const c = a+b;
+    expect(c).toBe(9);
+});
