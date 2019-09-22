49889f3d4347cd2cef80478993e5f228962132e5
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-09-22 01:05:18 -0300

    Start using Morgan as middleware

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,14 @@ const express = require('express');
 const models = require('./models');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 class App {
     constructor() {
         this.sequelize = models.sequelize;
         this.express = express();
         this.express.use(bodyParser.json());
+        this.express.use(morgan('dev'));
         this.express.use('/', routes);
     }
 
