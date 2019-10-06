f2454d390ad43a546e65618e962a1394e13e1a95
thomaz <thomaz.zandonotto@gmail.com>
2019-10-05 23:18:12 -0300

    Create a migration for post model

---
+++ src/database/migrations/20191006015210-create-post-table.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('post', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('post');
+  }
+};
