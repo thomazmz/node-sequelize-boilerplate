e9f2c1e31aba106cfb31340eb6bfbe0b3ae7cb37
thomaz <thomaz.zandonotto@gmail.com>
2019-10-24 00:27:18 -0300

    Change table User to have a role

--- src/infrastructure/database/migrations/20190918153001-create-user-table.js
+++ src/infrastructure/database/migrations/20190918153001-create-user-table.js
@@ -22,6 +22,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Role', key: 'id' }
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false
