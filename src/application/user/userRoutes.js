bf43578a711b78af2444eea279164f47d54209f6
thomaz <thomaz.zandonotto@gmail.com>
2019-10-24 22:56:35 -0300

    Remove Task domain

--- src/application/user/userRoutes.js
+++ src/application/user/userRoutes.js
@@ -3,7 +3,5 @@ const controller = require('../controllers/userController');
 const validators = require('../validators/userValidators');
 
 router.get('/:id', validators.findById, controller.findOneById);
-router.get('/:id/tasks', validators.findTasks, controller.findTasks);
-router.get('/:id/tasks/:taskId', validators.findTask, controller.findTask);
 
 module.exports = router;
