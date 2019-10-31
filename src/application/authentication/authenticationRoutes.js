a086c723d6c1a0dd62adba771581c5001065e4a9
thomaz <thomaz.zandonotto@gmail.com>
2019-10-31 05:25:25 -0300

    Temporarily clena up authorization and authentication

--- src/application/authentication/authenticationRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('./authenticationControllers');
-const validators = require('./authenticationRoutesValidators');
-
-router.post('/signin', validators.signIn, controller.signIn);
-router.post('/signup', validators.signUp, controller.signUp);
-router.post('/signup/:payload', controller.validate);
-
-module.exports = router;
--- src/application/authorization/authorizationRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('./authorizationController');
-
-router.get('/permisions', controller.findPermisions);
-router.get('/roles/:id', controller.findOneById);
-router.post('/roles', controller.createRole);
-
-module.exports = router;
