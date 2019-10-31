df723f54d9efdfa176a23ac5f41e9deae04b0897
thomaz <thomaz.zandonotto@gmail.com>
2019-10-31 05:17:55 -0300

    Include associations on database loader

--- src/infrastructure/database/index.js
+++ src/infrastructure/database/index.js
@@ -3,30 +3,36 @@ const path = require('path');
 const cls = require('cls-hooked');
 const Sequelize = require('sequelize');
 const config = require('./config');
+
 const domain = "../../domain"
-
 Sequelize.useCLS(cls.createNamespace('applicationNamespace'));
-
-const models = {
-	Sequelize : Sequelize,
-    sequelize : new Sequelize(config[process.env.NODE_ENVIRONMENT])
-}
+sequelize = new Sequelize(config[process.env.NODE_ENVIRONMENT])
+models = []
 
 const filePaths = getFilePaths(domain);
 
 filePaths.forEach(filePath => {
 	const Model = require(filePath);
 	if(isModel(Model)) {
-		const model = Model.init(models.sequelize, Sequelize);
+		const model = Model.init(sequelize, Sequelize);
 		models[model.name] = model;
 	}
 });
 
+Object.keys(models).forEach((modelName) => {
+    if (models[modelName].associate) {
+        models[modelName].associate(models);
+    }
+});
+
 function getFilePaths(targetPath, filePaths) {
+	
 	filePaths = filePaths || [];
 	const nodes = fs.readdirSync(path.join(__dirname, targetPath));
+	
 	nodes.forEach(node => {
 		const nodePath = path.join(__dirname, targetPath + "/" + node);
+
 		if(fs.statSync(nodePath).isDirectory()) {
 			getFilePaths(path.join(targetPath, node), filePaths);
 		} else if(node.slice(-3) === '.js') {
@@ -40,6 +46,5 @@ function isModel(fileContent) {
 	return (fileContent.prototype instanceof Sequelize.Model);
 }
 
-module.exports = models;
-
+module.exports = { Sequelize, sequelize, ...models };
 
