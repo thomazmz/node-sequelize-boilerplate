8164f51eab23c071ebe6869c3c753ec8055cb07c
thomaz <thomaz.zandonotto@gmail.com>
2019-11-01 12:24:59 -0300

    Rewrite model importing procedure

--- src/infrastructure/database/index.js
+++ src/infrastructure/database/index.js
@@ -3,48 +3,83 @@ const path = require('path');
 const cls = require('cls-hooked');
 const Sequelize = require('sequelize');
 const config = require('./config');
+const domain = '../../domain';
 
-const domain = "../../domain"
 Sequelize.useCLS(cls.createNamespace('applicationNamespace'));
 sequelize = new Sequelize(config[process.env.NODE_ENVIRONMENT])
-models = []
 
-const filePaths = getFilePaths(domain);
+const models = {};
 
-filePaths.forEach(filePath => {
-	const Model = require(filePath);
-	if(isModel(Model)) {
-		const model = Model.init(sequelize, Sequelize);
-		models[model.name] = model;
-	}
+const files = getFiles(domain);
+
+files.forEach(file => {
+	console.log(file);
+	const File = require(file);
+	const model = File.init(sequelize, Sequelize);
+	models[model.name] = model;
 });
 
-Object.keys(models).forEach((modelName) => {
-    if (models[modelName].associate) {
-        models[modelName].associate(models);
-    }
-});
+function getFiles(relativePath) {
 
-function getFilePaths(targetPath, filePaths) {
+	const paths = getPaths(path.join(__dirname, relativePath));
+
+	const files = paths.filter(path => {
+		const file = require(path);
+		return file.prototype instanceof Sequelize.Model;
+	});
+
+	return files;
+}
+
+function getPaths(globalPath, filePaths) {
 	
 	filePaths = filePaths || [];
-	const nodes = fs.readdirSync(path.join(__dirname, targetPath));
-	
-	nodes.forEach(node => {
-		const nodePath = path.join(__dirname, targetPath + "/" + node);
 
-		if(fs.statSync(nodePath).isDirectory()) {
-			getFilePaths(path.join(targetPath, node), filePaths);
-		} else if(node.slice(-3) === '.js') {
-			filePaths.push(path.join(targetPath, node));
+	fs.readdirSync(globalPath).forEach(file => {
+
+		const filePath = path.join(globalPath, file);
+
+		if(fs.statSync(filePath).isDirectory()) {
+			getPaths(filePath, filePaths);
+
+		} else if(file.slice(-3) === '.js') {
+			filePaths.push(filePath);
 		}
+
 	});
+
 	return filePaths;
 }
 
-function isModel(fileContent) {
-	return (fileContent.prototype instanceof Sequelize.Model);
-}
+Object.values(models)
+	.filter(model => typeof model.associate === "function")
+	.forEach(model => model.associate(models));
+  
+module.exports = { ...models, sequelize };
 
-module.exports = { Sequelize, sequelize, ...models };
 
+
+
+// const config = require('./config');
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(config[process.env.NODE_ENVIRONMENT])
+
+
+// const UserModel = require("../../domain/user/User");
+// const AuthorizationModel = require("../../domain/authorization/Authorization");
+
+// const models = {
+//   User: UserModel.init(sequelize, Sequelize),
+//   Authorization: AuthorizationModel.init(sequelize, Sequelize)
+// };
+
+// Object.values(models)
+//   .filter(model => typeof model.associate === "function")
+//   .forEach(model => model.associate(models));
+
+// const db = {
+//   ...models,
+//   sequelize
+// };
+
+// module.exports = db;
