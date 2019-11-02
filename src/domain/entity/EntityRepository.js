172ffb4fe43577ea89f3437b6f1d5a65fc643671
thomaz <thomaz.zandonotto@gmail.com>
2019-11-02 00:33:05 -0300

    Implement findAll and findAllOrderedBy

--- src/domain/entity/EntityRepository.js
+++ src/domain/entity/EntityRepository.js
@@ -4,6 +4,18 @@ class EntityRepository {
 		this._model = model;
 	}
 
+	findAllOrderedBy(attribute="id", offset, limit=100) {
+		return User.findAndCountAll({
+			order: [[attribute, 'ASC']],
+			limit: limit,
+			offset: offset
+		 });
+	}
+
+	findAll() {
+		return this._model.findAll();
+	}
+
 	build(parameters) {
 		return this._model.build(parameters);
 	}
