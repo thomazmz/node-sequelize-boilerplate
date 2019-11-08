2df6c0ecbc527b9c1c6864330dddef769b33d202
thomaz <thomaz.zandonotto@gmail.com>
2019-11-07 21:56:48 -0300

    Fix code formattng

--- src/application/authentication/authenticationController.js
+++ src/application/authentication/authenticationController.js
@@ -1,5 +1,6 @@
 const InvalidArgumentError = require('../../domain/error/InvalidArgumentError');
 const RequestError = require('../error/RequestError');
+
 const authenticationService = require('../../domain/authentication/AuthenticationService');
 const userRepository = require('../../domain/user/UserRepository');
 const cacheProvider = require('../../infrastructure/cache');
@@ -16,11 +17,11 @@ module.exports = {
 	signUp: (req, res, next) => {
 		cacheProvider.get(`USER_SIGN_UP_${req.body.username}`)
 		.then(user => !user ? userRepository.findOneByUsername(req.body.username) : 
-			new RequestError(422, { message : 'Username already in use' }).throw())
+			new RequestError(422, { message : 'Username already in use.' }).throw())
 		.then(user => !user ? userRepository.findOneByEmail(req.body.email) : 
-			new RequestError(422, { message : 'Username already in use' }).throw())
+			new RequestError(422, { message : 'Username already in use.' }).throw())
 		.then(user => !user ? authenticationService.createSignUpReference(req.body) : 
-			new RequestError(422, { message : 'Email already in use' }).throw())
+			new RequestError(422, { message : 'Email already in use.' }).throw())
 		.then(() => res.status(200).send())
 		.catch(err => next(err));
 	},
@@ -28,9 +29,9 @@ module.exports = {
 	validate: (req, res, next) => {
 		authenticationService.validateSignUpReference(req.params.payload)
 		.then(user => user.getBarearToken())
-		.then(barearToken => res.status(200).send(barearToken))
+		.then(token => res.status(200).send({ token }))
 		.catch(err => {
-			if (err instanceof InvalidArgumentError) next(new RequestError(422, { message : 'Invalid token' }));
+			if (err instanceof InvalidArgumentError) next(new RequestError(422, { message : 'Invalid token.' }));
 			else next(err);
 		});
 	}
